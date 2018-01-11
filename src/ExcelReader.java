c7e00dd282e3e9b0ec4ab5d291b7ef6e6978ef77
leegaeul <gaeul.lee@kt.com>
2018-01-11 13:25:18 +0900

    엑셀파일 읽기 개선

--- src/ExcelReader.java
+++ src/ExcelReader.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
@@ -30,20 +33,89 @@ public class ExcelReader {
 	}
 
 	/**
+	 * .xls확장자파일
 	 * 
-	 * 
+	 * @param File
 	 * @return void
 	 * @throws IOException
 	 */
-	public void readXls(File input) throws IOException {
-		// 파일을 읽기위해 엑셀파일을 가져온다
-		FileInputStream fis = new FileInputStream(input);
+	public ArrayList<HashMap<Integer, Object>> readXls(File input) throws IOException {
+		return readXls(new FileInputStream(input));
+	}
+
+	/**
+	 * .xls확장자파일
+	 * 
+	 * @param InputStream
+	 * @return void
+	 * @throws IOException
+	 */
+	public ArrayList<HashMap<Integer, Object>> readXls(InputStream input) throws IOException {
+		FileInputStream fis = (FileInputStream) input;
 		HSSFWorkbook workbook = new HSSFWorkbook(fis);
+		HSSFSheet sheet = workbook.getSheetAt(0);
+		ArrayList<HashMap<Integer, Object>> result = new ArrayList<>();
+		for (int rowindex = 1; rowindex < sheet.getPhysicalNumberOfRows(); rowindex++) {
+			HSSFRow row = sheet.getRow(rowindex);
+			HashMap<Integer, Object> map = new HashMap<>();
+			if (row != null) {
+				for (int columnindex = 0; columnindex <= row.getPhysicalNumberOfCells(); columnindex++) {
+					HSSFCell cell = row.getCell(columnindex);
+					if (cell != null)
+						map.put(columnindex, cell.getCellFormula().toString());
+				}
+			}
+			result.add(map);
+		}
+		return result;
+
+	}
+
+	/**
+	 * .xlsx확장자파일
+	 * 
+	 * @param File
+	 * @return void
+	 * @throws IOException
+	 */
+	public ArrayList<HashMap<Integer, Object>> readXlsx(File input) throws IOException {
+		return readXlsx(new FileInputStream(input));
+	}
+
+	/**
+	 * .xlsx확장자파일
+	 * 
+	 * @param InputStream
+	 * @return void
+	 * @throws IOException
+	 */
+	public ArrayList<HashMap<Integer, Object>> readXlsx(InputStream input) throws IOException {
+		FileInputStream fis = (FileInputStream) input;
+		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+		XSSFSheet sheet = workbook.getSheetAt(0);
+
+		printXSSFSheet(sheet);
+
+		ArrayList<HashMap<Integer, Object>> result = new ArrayList<>();
+		for (int rowindex = 1; rowindex < sheet.getPhysicalNumberOfRows(); rowindex++) {
+			XSSFRow row = sheet.getRow(rowindex);
+			HashMap<Integer, Object> map = new HashMap<>();
+			if (row != null) {
+				for (int columnindex = 0; columnindex <= row.getPhysicalNumberOfCells(); columnindex++) {
+					// 셀값을 읽는다
+					XSSFCell cell = row.getCell(columnindex);
+					if (cell != null)
+						map.put(columnindex, cell.getRawValue().toString());
+				}
+				result.add(map);
+			}
+		}
+		return result;
+	}
+
+	public void printHSSFSheet(HSSFSheet sheet) {
 		int rowindex = 0;
 		int columnindex = 0;
-		// 시트 수 (첫번째에만 존재하므로 0을 준다)
-		// 만약 각 시트를 읽기위해서는 FOR문을 한번더 돌려준다
-		HSSFSheet sheet = workbook.getSheetAt(0);
 		// 행의 수
 		int rows = sheet.getPhysicalNumberOfRows();
 		for (rowindex = 1; rowindex < rows; rowindex++) {
@@ -84,7 +156,6 @@ public class ExcelReader {
 				}
 			}
 		}
-
 	}
 
 	/**
@@ -92,16 +163,11 @@ public class ExcelReader {
 	 * @return void
 	 * @throws IOException
 	 */
-	public void readXlsx(File input) throws IOException {
-
-		// 파일을 읽기위해 엑셀파일을 가져온다
-		FileInputStream fis = new FileInputStream(input);
-		XSSFWorkbook workbook = new XSSFWorkbook(fis);
+	public void printXSSFSheet(XSSFSheet sheet) {
 		int rowindex = 0;
 		int columnindex = 0;
 		// 시트 수 (첫번째에만 존재하므로 0을 준다)
 		// 만약 각 시트를 읽기위해서는 FOR문을 한번더 돌려준다
-		XSSFSheet sheet = workbook.getSheetAt(0);
 
 		// 행의 수
 		int rows = sheet.getPhysicalNumberOfRows();
